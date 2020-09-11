d48706670e33091f8206d188e2dd90aa1629cb15
katou-raiden <emberl0new0lf@gmail.com>
2020-09-10 21:23:07 -0700

    Mejorado login y signup de la app accounts, en html, css, y js

--- concurso_matematica/accounts/urls.py
+++ concurso_matematica/accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 urlpatterns = [
   path('login/', login_view, name='login'),
   path('logout/', logout_view, name='logout'),
-  path('sign-up/', signUp_view, name='sign-up'),
+  path('sign-up/', signUp_view, name='signup'),
 
 ]
--- concurso_matematica/accounts/views.py
+++ concurso_matematica/accounts/views.py
@@ -10,43 +10,64 @@ from django.contrib import messages
 def login_view(request):
     
     if request.method == "POST":
+        #obtengo username y password del diccionario de POST
+
         username = request.POST.get('username')
         password = request.POST.get('password')
+        # autentico contra todos los backends de autentificacion
         user = authenticate(request, username=username, password=password)
         if user is not None:
+            #ligo la sesion del request a la instancia del usuario autentificado
             login(request, user)
             return redirect('home')
         else:
+            #mensaje al contexto global por si las credenciales son erroneas
             messages.error(request, 'Usuario o contransenna incorrectos')
+
+    #no hace falta contexto ya que obtengo directamente las credenciales un form personalizado
 
     return render(request, 'accounts/login.html', {})
 
 def logout_view(request):
+    #borro todos los datos de la sesion del request
     logout(request)
     return HttpResponse('You have logged out successfully')
 
 def signUp_view(request):
+    #los tres formularios de los tres modelos principales de cuenta de usuario
     user_form = UserForm()
     professor_form = ProfessorForm()
     student_form = StudentForm()
 
     if request.method == "POST":
+        
+        #si el envio de datos es por post, relleno todos los formularios con su 
+        #respectiva info
+
         user_form = UserForm(request.POST)
         professor_form = ProfessorForm(request.POST)
         student_form = StudentForm(request.POST)
-
+        
         if user_form.is_valid():
+            #Si se rellenaron todos los campos necesarios correctamente del modelo 
+            #usuario, entonces estamos listos para proceder con las extensiones del mismo
+            #El primero que tenga informacion proveida y correcta, es el procesado
+
             if professor_form.is_bound and professor_form.is_valid():
+                #profesor tiene datos y son correctos (literalmente)
                 new_user = user_form.save(commit=False)
                 new_user.set_password(request.POST.get('password'))
                 new_user.save()
+                #en estas tres lineas aseguro que la instancia user este salvada para
+                #que la extension tenga su referencia asegurada en la bd
                 new_professor = professor_form.save(commit=False)
                 new_professor.user = new_user
                 new_professor.save()
-
+                #modelo extendido salvado con exito y procedemos a la redireccion
                 return HttpResponse('Registrado con exito')
             
             elif student_form.is_bound and student_form.is_valid():
+                #Mismos procedimientos
                 new_user = user_form.save(commit=False)
                 new_user.set_password(request.POST.get('password'))
                 new_user.save()
@@ -56,7 +77,7 @@ def signUp_view(request):
 
                 return HttpResponse('Registrado con exito')
 
-    
+
     context = {
         'user_form' : user_form,
         'professor_form': professor_form,
