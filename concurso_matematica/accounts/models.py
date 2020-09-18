840220ab93ed108c582dd5005d9927d4a4a71369
katou-raiden <emberl0new0lf@gmail.com>
2020-09-18 02:25:42 -0700

    mend

--- concurso_matematica/accounts/models.py
+++ concurso_matematica/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from news.models import Notice 
 from django.core.validators import RegexValidator
 # Create your models here.
 
