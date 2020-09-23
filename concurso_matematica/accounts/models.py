35c46ad5c0c4ab5947a85af79c2af6a13d5105c4
katou-raiden <emberl0new0lf@gmail.com>
2020-09-23 01:28:15 -0700

    added login and sign up ui with javascript inside

--- concurso_matematica/accounts/models.py
+++ concurso_matematica/accounts/models.py
@@ -9,18 +9,24 @@ PROVINCE_REGEX = '48|47|7|45|42|43|51|41|33|32|31|24|23|22|21|46'
 PUI_REGEX = None
 
 class Profile(models.Model):
-    image = models.ImageField(upload_to="users/profile_pictures", null=True)
+
+    """Campos correspondientes a ambos modelos principales extendidos, nótese que no seran rellenados en los formularios, sino podran serlo durante la estancia del usuario, otros, como el dni sino lo son durante un tiempo determinado, se hara uso forzoso de la magia negra para rellenarlos automaticamente, aun si no lo son."""
+
+    avatar = models.ImageField(upload_to="users/profile_pictures", null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    phone_number = models.CharField(max_length=7, blank=True, default='')
-    private_number = models.CharField(max_length=9, blank=True, default='')
-    province = models.CharField(max_length=15, blank=True, default='')
-    
+    phone_number = models.CharField(max_length=7, blank=True, default='', null=True)
+    private_number = models.CharField(max_length=9, blank=True, default='', null=True)
+    province = models.CharField(max_length=15, blank=True, default='', null=True)
+    dni = models.CharField(max_length=11, blank='', default='', null=True)
+    home_address =models.CharField(max_length=40, default='', blank='', null=True)
     
 
 class Student(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     pui = models.CharField(max_length=20, blank=True, default='')
     grade = models.CharField(max_length=1, blank=True, default='')
+    score = models.IntegerField(default=0)
+    reputation = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.username
@@ -28,11 +34,13 @@ class Student(models.Model):
 class Professor(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     institute = models.CharField(max_length=20, blank=True, default='')
+    reputation = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
     
     def __str__(self):
         return self.user.username
 
 class SocialLink(models.Model):
     link = models.URLField(default='')
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
