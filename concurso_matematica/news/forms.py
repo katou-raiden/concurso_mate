0ecb25719e00101d7656d89d84be139761383b44
lustful-pavorus <banano027@gmail.com>
2020-09-27 17:58:08 -0500

    New view implementada

--- concurso_matematica/news/forms.py
+++ concurso_matematica/news/forms.py
@@ -13,11 +13,11 @@ class MainCommentForm(forms.ModelForm):
 
     class Meta:
         model = MainComment
-        exclude = ['upvotes', 'downvotes', 'date_mod', 'date_pub']
+        exclude = ['upvotes', 'downvotes', 'date_mod', 'date_pub','notice','user']
 
 
 class SubCommentForm(forms.ModelForm):
 
     class Meta:
         model = SubComment
-        exclude = ['upvotes', 'downvotes', 'date_mod', 'date_pub', 'main_comment']
+        exclude = ['upvotes', 'downvotes', 'date_mod', 'date_pub', 'main_comment','user']
--- concurso_matematica/news/views.py
+++ concurso_matematica/news/views.py
@@ -1,17 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
+from .forms import *
 # Create your views here.
 
 def new_view(request, pk):
-
-    new = Notice.objects.get(pk=pk)
+    main_form = MainCommentForm(prefix='main')
+    sub_form = SubCommentForm(prefix='sub')
+    new = get_object_or_404(Notice,pk=pk)
+    comments = MainComment.objects.filter(notice = new)
     context = {
-        'new' : new,
+        'main_form':main_form,
+        'sub_form':sub_form,
+        'new':new,
+        'comments':comments
     }
-    return render(request, 'news/new.html', context)
+    if request.method == 'POST':
+        main_form = MainCommentForm(request.POST,prefix='main')
+        sub_form = SubCommentForm(request.POST, prefix='sub')
+
+        if main_form.is_valid():
+            main = main_form.save(commit=False)
+            if request.user.is_authenticated:
+                main.user = request.user
+                main.notice = new
+                main.save()
+            else:
+                return redirect('login')    
+            return redirect('new',pk)
+        elif sub_form.is_valid():
+            sub = sub_form.save(commit=False)
+            if request.user.is_authenticated:
+                sub.user = request.user
+                sub.save()
+            else:
+                return redirect('login')
+            return redirect('new',pk)
+    else:
+        return render(request, 'news/new.html', context)
 
 def list_news_view(request):
+    news = Notice.objects.all()
     context = {
-        
+        'news':news
     }
-    return render(request, 'news/new.html', context)
+    return render(request, 'news/main.html', context)
+
+
