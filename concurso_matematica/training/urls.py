213b989129accd7904878933aeb562686967a809
lustful-pavorus <banano027@gmail.com>
2021-01-03 21:32:24 -0500

    cosas ahi

--- concurso_matematica/training/urls.py
+++ concurso_matematica/training/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', main_view, name='training'),
     path('test/',test_view),
-    path('exercise_post/', exercise_post_view, name='ex_post'),
+    #path('exercise_post/', exercise_post_view, name='ex_post'),
     path('<str:level>/', level_view, name='level_main' ),
     path('exercise/<int:pk>/', exercise_detail_view, name='exercise'),
     path('exercise_to_pdf/<int:pk>', render_pdf_view, name='render'),
